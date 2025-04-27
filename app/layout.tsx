c047180a8d72c1c758ed1b55051f3544eae59c7e
Gontier <alexis.gontier03@gmail.com>
2025-04-27 21:37:12 +0200

    Nextjs create

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import "@/app/globals.css";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="fr">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
